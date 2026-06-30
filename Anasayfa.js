ff2948bc5c18bbe893c6ab664a86e700dcbb4db6
Elitpeaper <78750921+Elitpeaper@users.noreply.github.com>
2026-06-30 20:28:30 +0300

    Add files via upload

---
+++ Anasayfa.js
@@ -0,0 +1,172 @@
+var title = document.querySelector(".panel1");
+var opa = "";
+var height = "";
+var visible = "";
+var padding = "";
+
+title.addEventListener("click", () => {
+	var titleEl = title
+		.parentNode
+		.querySelector(".content1")
+		.style;
+
+	if(titleEl.opacity == "1") {
+		opa = "0";
+		height = "0px";
+		visible = "hidden";
+		padding = "0px 10px 0px 10px";
+	} else {
+		height = "auto";
+		visible = "visible";
+		opa = "1";
+		padding = "10px";
+	}
+	
+		
+		titleEl.opacity = opa
+		titleEl.height = height;
+		titleEl.visibility = visible;
+		titleEl.padding = padding;
+})
+
+
+
+
+
+
+
+
+var title = document.querySelector(".panel2");
+var opa = "";
+var height = "";
+var visible = "";
+var padding = "";
+
+title.addEventListener("click", () => {
+	var titleEl = title
+		.parentNode
+		.querySelector(".content2")
+		.style;
+
+	if(titleEl.opacity == "1") {
+		opa = "0";
+		height = "0px";
+		visible = "hidden";
+		padding = "0px 10px 0px 10px";
+	} else {
+		height = "auto";
+		visible = "visible";
+		opa = "1";
+		padding = "10px";
+	}
+	
+		
+		titleEl.opacity = opa
+		titleEl.height = height;
+		titleEl.visibility = visible;
+		titleEl.padding = padding;
+})
+
+
+
+
+
+
+var title = document.querySelector(".panel3");
+var opa = "";
+var height = "";
+var visible = "";
+var padding = "";
+
+title.addEventListener("click", () => {
+	var titleEl = title
+		.parentNode
+		.querySelector(".content3")
+		.style;
+
+	if(titleEl.opacity == "1") {
+		opa = "0";
+		height = "0px";
+		visible = "hidden";
+		padding = "0px 10px 0px 10px";
+	} else {
+		height = "auto";
+		visible = "visible";
+		opa = "1";
+		padding = "10px";
+	}
+	
+		
+		titleEl.opacity = opa
+		titleEl.height = height;
+		titleEl.visibility = visible;
+		titleEl.padding = padding;
+})
+
+
+
+var title = document.querySelector(".panel4");
+var opa = "";
+var height = "";
+var visible = "";
+var padding = "";
+
+title.addEventListener("click", () => {
+	var titleEl = title
+		.parentNode
+		.querySelector(".content4")
+		.style;
+
+	if(titleEl.opacity == "1") {
+		opa = "0";
+		height = "0px";
+		visible = "hidden";
+		padding = "0px 10px 0px 10px";
+	} else {
+		height = "auto";
+		visible = "visible";
+		opa = "1";
+		padding = "10px";
+	}
+	
+		
+		titleEl.opacity = opa
+		titleEl.height = height;
+		titleEl.visibility = visible;
+		titleEl.padding = padding;
+})
+
+
+
+var title = document.querySelector(".panel5");
+var opa = "";
+var height = "";
+var visible = "";
+var padding = "";
+
+title.addEventListener("click", () => {
+	var titleEl = title
+		.parentNode
+		.querySelector(".content5")
+		.style;
+
+	if(titleEl.opacity == "1") {
+		opa = "0";
+		height = "0px";
+		visible = "hidden";
+		padding = "0px 10px 0px 10px";
+	} else {
+		height = "auto";
+		visible = "visible";
+		opa = "1";
+		padding = "10px";
+	}
+	
+		
+		titleEl.opacity = opa
+		titleEl.height = height;
+		titleEl.visibility = visible;
+		titleEl.padding = padding;
+})
+
+
